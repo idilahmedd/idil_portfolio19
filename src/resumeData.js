b67c611b512c97a3cf2609e83bb71c6dcc4abf2f
Idilahmed <idil@mac-books-MacBook-Pro.local>
2019-08-28 15:09:32 -0700

    initial

---
+++ src/resumeData.js
@@ -0,0 +1,84 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Idil Ahmed",
+    "role": "Software Developer",
+    "linkedinId":"Your LinkedIn Id",
+    "roleDescription": "I like dabbling in various parts of frontend and backend development and like to learn about new technologies.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/idilahmedd/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/idilahmedd",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"resume",
+          "url":"http://twitter.com/rbhatia46",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"Personable, driven, and multifaceted professional who succeeds at providing solutions to complex problems. I am a highly analytical results driven Full Stack Developer using unique methods to identify business needs in order to improve internal and external processes and to maximize efficiency. I am driven to solve complex challenges and provide solutions for both businesses and clients.",
+    "address":"Seattle, WA",
+    "website":"https://idilahmed.dev",
+    "education":[
+      {
+        "UniversityName":"General Assembly Seattle",
+        "specialization":"Software Engineering Immersive",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2019",
+        "Achievements":"Graduate"
+      },
+      {
+        "UniversityName":"University of Washington",
+        "specialization":"BA Anthropology",
+        "MonthOfPassing":"June",
+        "YearOfPassing":"2015",
+        "Achievements":"BA"
+      }
+    ],
+    "skillsDescription":"",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"Node"
+      },
+      {
+        "skillname":"JavaScript"
+      },
+      {
+        "skillname":"Phython"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"Kause",
+        "description":"Full Stack Web Application",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"KidConnect",
+        "description":"Full Stack Web Application",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"Speed",
+        "description":"Front-End Web Game",  
+        "imgurl":"images/portfolio/project2.png"
+      }
+      
+    ]
+  }
+  
+  export default resumeData
