dded282077ec2a28966c28fd54cd820e5bb34d11
Idilahmed <idil@mac-books-MacBook-Pro.local>
2019-09-11 13:35:56 -0700

    added particles.js to cover

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,49 +1,74 @@
 import React, { Component } from 'react';
+import Particles from 'react-particles-js';
 export default class Header extends Component {
   render() {
     let resumeData = this.props.resumeData;
+    var moving = < Particles className="particles"
+      params={{
+        "particles": {
+          "number": {
+            "value": 50
+          },
+          "size": {
+            "value": 3
+          }
+        },
+        "interactivity": {
+          "events": {
+            "onhover": {
+              "enable": true,
+              "mode": "repulse"
+            }
+          }
+        }
+      }} />
+      
+      
     return (
       <React.Fragment>
-      
-      <header id="home">
-         <nav id="nav-wrap">
-            <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
-          {/* <a className="mobile-btn" href="#" title="Hide navigation">Hide navigation</a> */}
-            <ul id="nav" className="nav">
-               <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
-               <li><a className="smoothscroll" href="#about">About</a></li>
-             <li><a className="smoothscroll" href="#resume">Resume</a></li>
-               <li><a className="smoothscroll" href="#portfolio">Work</a></li>
-               <li><a className="smoothscroll" href="#contact">Contact</a></li>
-            </ul>
-         </nav>
+        <div>
+          {moving}
 
-         <div className="row banner">
-            <div className="banner-text">
-               <h1 className="responsive-headline">I am <span style={{color:'rgb(196, 89, 7)'}}>{resumeData.name}</span>.</h1>
-               <h3 style={{color:'#fff', fontFamily:'sans-serif '}}>I am a {resumeData.role}.{resumeData.roleDescription}
-               </h3>
-               <hr/>
-               <ul className="social">
+          <header id="home">
+            <nav id="nav-wrap">
+              <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
+              {/* <a className="mobile-btn" href="#" title="Hide navigation">Hide navigation</a> */}
+              <ul id="nav" className="nav">
+                <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
+                <li><a className="smoothscroll" href="#about">About</a></li>
+                <li><a className="smoothscroll" href="#resume">Resume</a></li>
+                <li><a className="smoothscroll" href="#portfolio">Work</a></li>
+                <li><a className="smoothscroll" href="#contact">Contact</a></li>
+              </ul>
+            </nav>
+
+            <div className="row banner">
+              <div className="banner-text">
+                <h1 className="responsive-headline">I am <span style={{ color: 'rgb(196, 89, 7)' }}>{resumeData.name}</span>.</h1>
+                <h3 style={{ color: '#fff', fontFamily: 'sans-serif ' }}>I am a {resumeData.role}.{resumeData.roleDescription}
+                </h3>
+                <hr />
+                <ul className="social">
                   {
-                    resumeData.socialLinks && resumeData.socialLinks.map(item =>{
-                      return(
-                              <li key={item.name}>
-                                <a href={item.url} rel="noopener noreferrer" target="_blank"><i className={item.className}></i></a>
-                              </li>
-                            )
-                          }
+                    resumeData.socialLinks && resumeData.socialLinks.map(item => {
+                      return (
+                        <li key={item.name}>
+                          <a href={item.url} rel="noopener noreferrer" target="_blank"><i className={item.className}></i></a>
+                        </li>
+                      )
+                    }
                     )
                   }
-               </ul>
+                </ul>
+              </div>
             </div>
-         </div>
 
-         <p className="scrolldown">
-            <a className="smoothscroll" href="#about"><i className="icon-down-circle"></i></a>
-         </p>
+            <p className="scrolldown">
+              <a className="smoothscroll" href="#about"><i className="icon-down-circle"></i></a>
+            </p>
 
-      </header>
+          </header>
+        </div>
       </React.Fragment>
     );
   }
