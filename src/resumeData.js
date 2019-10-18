0d8e47fccb860f8740d339bcf0ecdb3f3c13130e
Idilahmed <idil@mac-books-MacBook-Pro.local>
2019-10-17 21:09:52 -0700

    resume v3

--- src/resumeData.js
+++ src/resumeData.js
@@ -22,7 +22,7 @@ let resumeData = {
         },
         {
           "name":"Resume",
-          "url":"https://drive.google.com/file/d/16DCDqwrPDrWeEziC9ysdf-PxA54h_IgU/view?usp=sharing",
+          "url":"https://drive.google.com/file/d/1QyLoECTDO6E_TvN_Wj0Gi21yEehAX1vj/view?usp=sharing",
           "className":"fa fa-file"
         }
       ],
